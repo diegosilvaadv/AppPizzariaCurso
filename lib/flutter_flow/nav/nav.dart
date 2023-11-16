e910fbf2c1096d9901197063891122fec22446d7
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-16 19:17:37 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/nav/nav.dart
+++ lib/flutter_flow/nav/nav.dart
@@ -136,6 +136,11 @@ GoRouter createRouter(AppStateNotifier appStateNotifier) => GoRouter(
           name: 'pizza2sab16GG',
           path: '/pizza2sab16GG',
           builder: (context, params) => Pizza2sab16GGWidget(),
+        ),
+        FFRoute(
+          name: 'pizza4sab8GG',
+          path: '/pizza4sab8GG',
+          builder: (context, params) => Pizza4sab8GGWidget(),
         )
       ].map((r) => r.toRoute(appStateNotifier)).toList(),
     );
